68cd5816e44390971a6fc2e21076dfb92fdf0881
Rezonanz <psydeus@gmail.com>
2022-10-07 19:02:52 +0300

    move to another repo

--- YSceneManagement/Core/Runtime/YSceneAssetReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.AddressableAssets;
-
-namespace YellowJelloGames.YToolbox.YSceneManagement.Runtime
-{
-    [System.Serializable]
-    public partial class YSceneAssetReference : AssetReference
-    {
-    }
-}
